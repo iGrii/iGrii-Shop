078e689dad7a001ac0839be9891c7d3b5d769a2a
iGrii <tu-correo-de-github@example.com>
2025-10-31 00:11:49 -0500

    Adaptar proyecto para correr solo en Render

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
-const mysql = require('mysql2');
+// db.js
+const { Pool } = require('pg'); // Usamos PostgreSQL
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',       // cambia si tu usuario MySQL es otro
-  password: '',        // tu contraseña de MySQL
-  database: 'igrishop'
+const pool = new Pool({
+  host: process.env.DB_HOST || 'dpg-d423r9re5dus73bdhi8g-a.oregon-postgres.render.com',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || 'igriishopdb',
+  user: process.env.DB_USER || 'igriishopdb_user',
+  password: process.env.DB_PASSWORD || 'bgCVZ1FZys73Ul6MDZjKQGXY7WRrAY5X',
 });
 
-const promisePool = pool.promise();
-module.exports = promisePool;
+pool.connect()
+  .then(() => console.log('Conectado a PostgreSQL en Render'))
+  .catch(err => console.error('Error al conectar a PostgreSQL', err));
+
+module.exports = pool;
